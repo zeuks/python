891c61cc5f3d2f57d0a9b5235663afd923fa31b4
Jason Ham <jasonham16@gmail.com>
2018-01-21 12:39:16 +0900

    matrix sum edited

--- matrix_sum_1.py
+++ matrix_sum_1.py
@@ -1,11 +1,8 @@
-A = [[3,4,5],
-[6,7,8],
-[1,3,4],
-[2,4,6]]
+A = [[3,4,5],[6,7,8],[1,3,4],[2,4,6]]
 
 sum_A = 0
-for row in A:
-    for items in row:
-        sum_A += items
+for row in range(4):
+    for items in range(3):
+        sum_A += A[row][items]
 
 print (sum_A)
