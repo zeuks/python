611290b4096efbcdaf5c20e5951296f461e51e9b
Jason Ham <jasonham16@gmail.com>
2018-01-28 20:09:04 +0900

    matrix multiplication 3 uploaded.

--- matrix_multiplication_3.py
+++ matrix_multiplication_3.py
@@ -1,39 +1,15 @@
 import numpy as np
-'''
-while True:
-    if a_row != b_col:
-        print ("Error. Please input again.")
-        a_col, a_row = [int(n) for n in input("Enter the list A (column,row): ").split(',')]
-        b_col, b_row = [int(n) for n in input("Enter the list B (column,row): ").split(',')]
-    else:
-        np.random.seed(1)
-        a = np.random.randn(a_col*a_row).reshape(a_col,a_row)
-        b = np.random.randn(b_col*b_row).reshape(b_col,b_row)
-        break
-        '''
+
 a_col, a_row = [int(n) for n in input("Enter the list A (column,row): ").split(',')]
 b_col, b_row = [int(n) for n in input("Enter the list B (column,row): ").split(',')]
 np.random.seed(1)
 a = np.random.randn(a_col*a_row).reshape(a_col,a_row)
 b = np.random.randn(b_col*b_row).reshape(b_col,b_row)
 
-def error(er):
-    if er == 1:
-        a_col, a_row = [int(n) for n in input("Enter the list A (column,row): ").split(',')]
-        b_col, b_row = [int(n) for n in input("Enter the list B (column,row): ").split(',')]
-        np.random.seed(1)
-        a = np.random.randn(a_col*a_row).reshape(a_col,a_row)
-        b = np.random.randn(b_col*b_row).reshape(b_col,b_row)
-        return (matrix_multiplication(a,b))
-
-print (a)
-print (b)
-#result lists
-C = [[] for x in range(a_col)]
-
 def matrix_multiplication (A, B):
+    C = [[] for x in range(len(A))]
     if len(A[0]) != len(B):
-        return -1
+        return (-1)
     else:
         for a in range(len(A)): # loops through number of rows
             for b in range(len(B[0])): # loops through number of elements
@@ -45,21 +21,27 @@ def matrix_multiplication (A, B):
 
 while True:
     print("\n")
-    print("Loading... Please wait. \n")
-    if (matrix_multiplication(a,b)) == -1:
-        print ("Error")
-        error(1)
+    print("Loading... Please wait.")
+    print ("\n")
+    if matrix_multiplication(a,b) == -1:
+        print ("ERROR")
+        print ("\n")
+        a_col, a_row = [int(n) for n in input("Enter the list A (column,row): ").split(',')]
+        b_col, b_row = [int(n) for n in input("Enter the list B (column,row): ").split(',')]
+        np.random.seed(1)
+        a = np.random.randn(a_col*a_row).reshape(a_col,a_row)
+        b = np.random.randn(b_col*b_row).reshape(b_col,b_row)
     else:
-        print ("Result: \n %s" % (matrix_multiplication(a,b)))
+        C = matrix_multiplication(a,b)
         correct_C = a.dot(b)
+        print ("Result: \n %s" % (C))
+        print ("\n")
+        print ("Result (column,row): %s, %s" % (len(C), len(C[0])))
+        print ("\n")
         break
-
-print ("\n")
-print ("Result (column,row): %s, %s" % (len(C), len(C[0])))
-print ("\n")
 
 # checks if the two lists are the same
 print ("Checking if the answer is correct:")
-print(C[2][3])
-print(correct_C[2][3])
+print(C[2][2])
+print(correct_C[2][2])
 print ("\n")
